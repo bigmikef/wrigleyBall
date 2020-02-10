f10d125278da0f331ecf117c9460694597a1d926
Mike Forsberg <mike.forsberg@hpe.com>
2020-02-10 12:43:35 -0600

    A crude solution to advancing runners.

--- field.go
+++ field.go
@@ -12,9 +12,9 @@ const (
 
 // Field is the game state
 type Field struct {
-	b1 bool
-	b2 bool
-	b3 bool
+	b1 *base
+	b2 *base
+	b3 *base
 
 	balls   int8
 	strikes int8
@@ -25,24 +25,63 @@ type Field struct {
 	score [2]int
 }
 
-func (f *Field) advRunners(nBases uint8, runner bool) error {
+// NewField creates a field
+func NewField() *Field {
+	f := Field{}
+	f.b3 = newBase(nil)
+	f.b2 = newBase(f.b3)
+	f.b1 = newBase(f.b2)
+	return &f
+}
+
+// Base is a place where a runner can land.
+type base struct {
+	on bool
+	n  *base
+}
+
+func (b *base) adv(r bool) int {
+	var ret int
+	if b.on {
+		if b.n != nil {
+			ret = b.n.adv(b.on)
+		} else {
+			ret = 1
+		}
+	}
+	b.on = r
+	return ret
+}
+
+// newBase creates a base for the field
+func newBase(nextBase *base) *base {
+	return &base{on: false, n: nextBase}
+}
+
+// AdvRunners force advances the runners, adding a runner to the base path if runner is true, incrementing the score of the team that is at bat.
+func (f *Field) AdvRunners(nBases uint8, runner bool) error {
 	if nBases > 4 || nBases < 1 {
 		return fmt.Errorf("nBases must be 1 - 4 inclusive - %d", nBases)
 	}
 
-	var ret = 0
-	for n := 1; n <= int(nBases); n++ {
-		if f.b3 {
-			ret++
-		}
-		f.b3 = f.b2
-		f.b2 = f.b1
-		if n == 1 {
-			f.b1 = runner
-		} else {
-			f.b1 = false
-		}
+	var score int
+	if nBases == 1 {
+		score = f.b1.adv(true)
+	} else if nBases == 2 {
+		score = f.b1.adv(true)
+		score += f.b1.adv(false)
+	} else if nBases == 3 {
+		score = f.b1.adv(true)
+		score += f.b1.adv(false)
+		score += f.b2.adv(false)
+	} else if nBases == 4 {
+		score = f.b1.adv(true)
+		score += f.b1.adv(false)
+		score += f.b2.adv(false)
+		score += f.b3.adv(false)
 	}
+
+	f.score[f.atBat] += score
 
 	return nil
 }
--- field_test.go
+++ field_test.go
@@ -7,49 +7,77 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func checkRunners(t *testing.T, f Field, first, second, third bool) {
-	assert.Equal(t, f.b1, first, fmt.Sprintf("First base incorrect - %t, %t, %t", f.b1, f.b2, f.b3))
-	assert.Equal(t, f.b2, second, fmt.Sprintf("Second base incorrect - %t, %t, %t", f.b1, f.b2, f.b3))
-	assert.Equal(t, f.b3, third, fmt.Sprintf("Third base incorrect - %t, %t, %t", f.b1, f.b2, f.b3))
+func checkRunners(t *testing.T, f *Field, first, second, third bool) {
+	assert.Equal(t, f.b1.on, first, fmt.Sprintf("First base incorrect - 1st:%t, 2nd:%t, 3rd:%t", f.b1.on, f.b2.on, f.b3.on))
+	assert.Equal(t, f.b2.on, second, fmt.Sprintf("Second base incorrect - 1st:%t, 2nd:%t, 3rd:%t", f.b1.on, f.b2.on, f.b3.on))
+	assert.Equal(t, f.b3.on, third, fmt.Sprintf("Third base incorrect - 1st:%t, 2nd:%t, 3rd:%t", f.b1.on, f.b2.on, f.b3.on))
 }
 
 // TestAdvRunnersSingleNoManOn checks that a single places a runner on first.
 func TestAdvRunnersSingleNoManOn(t *testing.T) {
-	var f Field
+	f := NewField()
 
-	f.advRunners(1, true)
+	f.AdvRunners(1, true)
 
 	checkRunners(t, f, true, false, false)
 	assert.Equal(t, f.score[f.atBat], 0)
 }
 
-// TestAdvRunnersDoubleNoManOn checks that a single places a runner on first.
+// TestAdvRunnersDoubleNoManOn checks that a double places a runner on second.
 func TestAdvRunnersDoubleNoManOn(t *testing.T) {
-	var f Field
+	f := NewField()
 
-	f.advRunners(2, true)
+	f.AdvRunners(2, true)
 
 	checkRunners(t, f, false, true, false)
 	assert.Equal(t, f.score[f.atBat], 0)
 }
 
-// TestAdvRunnersTripleNoManOn checks that a single places a runner on first.
+// TestAdvRunnersTripleNoManOn checks that a triple places a runner on third.
 func TestAdvRunnersTripleNoManOn(t *testing.T) {
-	var f Field
+	f := NewField()
 
-	f.advRunners(3, true)
+	f.AdvRunners(3, true)
 
 	checkRunners(t, f, false, false, true)
 	assert.Equal(t, f.score[f.atBat], 0)
 }
 
-// Does not work with current architecture.
-// // TestAdvRunnersDoubleFirstAndThird checks that a single places a runner on first.
-// func TestAdvRunnersDoubleFirstAndThird(t *testing.T) {
-// 	var f Field
+// TestAdvRunnersSingleFirstAndThird checks that a single places a runner on first and only advances runner on first.  Result should be bases loaded.
+func TestAdvRunnersSingleFirstAndThird(t *testing.T) {
+	f := NewField()
 
-// 	f.advRunners(3, true)
+	f.b1.on = true
+	f.b3.on = true
 
-// 	checkRunners(t, f, false, false, true)
-// 	assert.Equal(t, f.score[f.atBat], 0)
-// }
+	f.AdvRunners(1, true)
+
+	checkRunners(t, f, true, true, true)
+	assert.Equal(t, f.score[f.atBat], 0)
+}
+
+// TestAdvRunnersDoubleFirstAndThird checks that a double places a runner on second and runner on third scores.  Result should be 2nd and 3rd loaded.
+func TestAdvRunnersDoubleFirstAndThird(t *testing.T) {
+	f := NewField()
+
+	f.b1.on = true
+	f.b3.on = true
+
+	f.AdvRunners(2, true)
+
+	checkRunners(t, f, false, true, true)
+	assert.Equal(t, f.score[f.atBat], 1)
+}
+
+// TestAdvRunnersHomeRunFirstAndThird checks that a home run clears the bases.  Three runs should score.
+func TestAdvRunnersHomeRunFirstAndThird(t *testing.T) {
+	f := NewField()
+
+	f.b1.on = true
+	f.b3.on = true
+
+	f.AdvRunners(4, true)
+
+	checkRunners(t, f, false, false, false)
+	assert.Equal(t, f.score[f.atBat], 3)
+}
